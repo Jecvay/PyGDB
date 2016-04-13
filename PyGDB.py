231ff5fa9cfe55a693ab95d8a8fcc12c1afb52ee
Jecvay <ljwjiewei@gmail.com>
2016-04-13 19:42:29 +0800

    Add cmd parser.

--- PyGDB.py
+++ PyGDB.py
@@ -1,7 +1,19 @@
-from gdblib.StateGDB import StateGDB
+# coding=utf-8
 
-gdb = StateGDB("x1")
-result = gdb.question("start")
+import argparse
 
-
-print result
+if __name__ == "__main__":
+    # 配置命令行
+    cmd_parser = argparse.ArgumentParser(description="Python-GDB Test Suite")
+    cmd_sub_parser = cmd_parser.add_subparsers(title="sub command", dest="sub_cmd")
+    parser_test = cmd_sub_parser.add_parser('test', description="haha")
+    parser_gendata = cmd_sub_parser.add_parser('gendata', description="Generate data in the basis of config file.")
+    parser_gendata.add_argument("filename", help='The name of the config file (in the ./config/ direction.)')
+    cmd_args = cmd_parser.parse_args()
+    if cmd_args.sub_cmd == "test":
+        print("测试!")
+    elif cmd_args.sub_cmd == "gendata":
+        print("生成! " + cmd_args.filename)
+    print "显示内容: " + cmd_args.sub_cmd + "  <----"
+else:
+    raise Exception("This program isn't a module.")
--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # PyGDB
 Python 调用 GDB 的一个很方便使用的东东
 
+
+## 使用说明
+- 配置公共配置文件 ./config/config.yml 本配置只需要设置一次, 多个项目共享使用
+- 测试数据库连接 ./pygdb.py test
+- 配置项目配置文件 ./config/project.yml 其中 project.yml 文件名自选, 具体配置可以参考 project_example.yml
+- 执行项目数据生成程序 ./pygdb.py gendata project.yml 其中 project.yml 为 ./config/ 目录下的项目配置文件
+
+
 ## 工作流
 
 ### 1. 产生项目数据
@@ -12,3 +20,9 @@ Python 调用 GDB 的一个很方便使用的东东
 - 读取数据库, 产生基本信息
 - 封装 API, 对数据库数据进行组合, 处理, 并返回
 - 在封装 API 的基础上进行 Restful API 开发
+
+
+## 第三方库
+- [pymysql] Python MySQL: https://github.com/PyMySQL/PyMySQL 安装: pip install pymysql
+- [pyYaml] Python Yaml: http://pyyaml.org/ 安装: pip intall pyyaml
+
